f5daf0562e5a66ecccc63beb0de5697aaabc21e5
Joaquin <joaquinserra94@gmail.com>
2024-05-05 18:19:49 +0200

    Actividad clase

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { Link } from "react-scroll";
 import styles from './Navbar.module.css';
 import { AiOutlineClose, AiOutlineMenu } from "react-icons/ai";   //Aqui deberiamos instalar las 3 rayitas del menu, pero me da error
@@ -7,22 +7,44 @@ import { AiOutlineClose, AiOutlineMenu } from "react-icons/ai";   //Aqui deberia
 
 const Navbar = () => {
     const [navBarOpen, setNavbarOpen] = useState(false);
+    const [windowDimension, setWindowDimension] = useState({ //Aqui detectamos el tamaño de la pantalla para que nuestra app se ajuste a ella
+      width: window.innerWidth,
+      height: window.innerHeight,
+    });
+
+    const detectDimension = () => { //toma el estado del usestate de window y setea la dimension de la pantalla
+      setWindowDimension({
+        width: window.innerWidth,
+        height: window.innerHeight
+      })
+
+    }
+
+    useEffect(() =>{              //se ejecuta despues del primer renderizado y despues de cada actualizacion
+        window.addEventListener('resize', detectDimension)
+        windowDimension.width > 800 && setNavbarOpen(false)
+        return () =>{
+          window.removeEventListener('resize', detectDimension)
+        }
+    },[windowDimension])
+
+
     const links=[                                   //Seguidamente indicaremos los dintintos links que tendrá nuestro Navbar
         {                                           //En el caso de que sea necesario agregar mas links, poner id:x y debajo el link
             id:1,
-            link: "Home",
+            link: "Inicio",
         },
         {
             id:2,
-            link: "Services",
+            link: "Árboles Frutales",
         },
         {
             id:3,
-            link: "HowWeWork",
+            link: "Flores",
         },
         {
             id:4,
-            link: "Benefits",
+            link: "Contacto",
         },
 
         
@@ -32,23 +54,41 @@ const Navbar = () => {
 //En el caso de que el link sea HowWeWork, se mostrará How we work hacemos la pregunta con ?
 //En el caso de que el link sea diferente a HowWeWork, se mostrará el link por eso ponemos :
   return (
-    <div className ={navBarOpen === true ? styles.navBar : styles.navOpen}> 
-      <p>TESOROS ARGENTINOS | Frutales y Flores</p>                                     
-      { !navBarOpen ? (
-          <AiOutlineMenu onClick = {() => setNavbarOpen(!navBarOpen)} size={25} /> //con esto indico que pase lo contrario, si esta en falso se cambie a verdadero.
+    <div className ={!navBarOpen === true ? styles.navBar : styles.navOpen}>  
+      {!navBarOpen && <p className={styles.logo}>TESOROS ARGENTINOS | Frutales y Flores</p>}                                   
+      {!navBarOpen && windowDimension.width < 800 ? (
+          <AiOutlineMenu 
+          onClick = {() => setNavbarOpen(!navBarOpen)} 
+          size={25} 
+          /> //CONTROLAMOS LA X DE CLOSE, con esto indico que pase lo contrario, si esta en falso se cambie a verdadero.
       ) : (
-      <AiOutlineClose onClick = {() => setNavbarOpen(!navBarOpen)} size={25}/> 
+        windowDimension.width < 800 && (
+          <AiOutlineClose 
+            onClick = {() => setNavbarOpen(!navBarOpen)} 
+            color='#f1f1f1' //color blanco de la X
+            size={25}
+            /> //CONTROLAMOS LA X DE CLOSE
+        )
       )}
-      {
-        navBarOpen &&
-        <ul>                                  
+      {navBarOpen || (
+        windowDimension.width > 800 && (
+        <ul className= {styles.linksContainer}>                                  
         {links.map((x) => (
           <div>
-            <Link>{x.link === "HowWeWork" ? "How we work" : x.link}</Link>
+            <Link
+            onClick={() => setNavbarOpen(false)} //para que cuando hagamos clik en una opcion dentro de la barra, la barra se cierre
+             to={x.link}
+             smooth
+             duration = {500}
+             className={styles.navLink}
+            >
+              {x.link === "HowWeWork" ? "How we work" : x.link}
+            </Link>
+            <div className={styles.border}></div>
           </div>
         ))}
       </ul>
-      }
+      ))}
     </div>
   );
 };
--- src/Screens/HowWeWork.js
+++ src/Screens/HowWeWork.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 const HowWeWork = () => {
   return (
-    <div>HowWeWork</div>
+    <div>Flores</div>
   )
 }
 
