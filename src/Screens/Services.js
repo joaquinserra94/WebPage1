a15068830059165910df6914e1d946e04a45b336
Joaquin <joaquinserra94@gmail.com>
2024-05-05 22:45:33 +0200

    Actividad clase

--- src/Screens/Services.js
+++ src/Screens/Services.js
@@ -1,9 +1,29 @@
-import React from 'react'
+import React from 'react';
+import styles from "./Services.module.css";
 
 const Services = () => {
-  return (
-    <div>Árboles Frutales</div>
-  )
-}
+  const imageUrl = 'https://miro.medium.com/v2/resize:fit:1400/format:webp/1*kd5Zvu-bD4aQqzH9V_tMEw.jpeg'; 
 
-export default Services
+  return (
+    <div name="Services" className={styles.services}>
+      <p>
+        <b>Arboles frutales <br />caracteristicos de la región</b></p>
+
+      <p>CHILTO</p>
+      <p5>El chilto es una especie originaria de América Central y del Sur que fue
+domesticada y cultivada por las poblaciones nativas antes de la introducción
+de otros cultivos por la colonia. Actualmente es producido a nivel local, en
+huertas y quintas, por comunidades que lo incluyen en su alimentación y
+como un hábito heredado de sus antepasados.</p5>
+
+      <img 
+      className={styles.webImage}
+      src={imageUrl}
+      ></img>
+    </div>
+  );
+};
+
+export default Services;
+
+
